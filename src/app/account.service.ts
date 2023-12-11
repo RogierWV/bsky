eeb8e6ba21f753fa20fcfacfe9cd083a35e266b5
Rogier Velting <rogiervelting@gmail.com>
2023-12-11 11:06:38 +0100

    Now saving login state

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AtpService } from './atp.service';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
+import { AtpSessionData } from '@atproto/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  did: string|undefined;
   loggedIn : boolean = false;
   loggedInChange : Subject<boolean> = new Subject<boolean>();
 
@@ -15,6 +15,19 @@ export class AccountService {
     private atp: AtpService
   ) {
     this.loggedInChange.subscribe((v) => this.loggedIn = v);
+    const sessData = localStorage.getItem("bskySession") || "{}";
+    const sessDataParsed = JSON.parse(sessData);
+    if(this.isAtpSessionData(sessDataParsed)) {
+      const res = this.atp.agent.resumeSession(sessDataParsed);
+      res.then(r => {if(r.success) this.loggedInChange.next(true)});
+    }
+  }
+
+  private isAtpSessionData(o: any): o is AtpSessionData {
+    return "refreshJwt" in o && 
+      "accessJwt" in o &&
+      "handle" in o &&
+      "did" in o
   }
 
   async login(email: string|null, pass: string|null) {
@@ -23,8 +36,8 @@ export class AccountService {
         identifier: email,
         password: pass
       });
-      this.did = this.atp.agent.session?.did;
       this.loggedInChange.next(true);
     }
   }
 }
+
--- src/app/atp.service.ts
+++ src/app/atp.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { BskyAgent } from '@atproto/api'
+import { AtpSessionData, AtpSessionEvent, BskyAgent } from '@atproto/api'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AtpService {
-  public agent = new BskyAgent({service: 'https://bsky.social/'});
+  public agent = new BskyAgent({
+    service: 'https://bsky.social/',
+    persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
+      localStorage.setItem("bskySession", JSON.stringify(sess));
+    }
+  });
   constructor() { }
   // async login(email: string, pass: string) {
   //   await this.agent.login({identifier:email, password:pass});
@@ -15,3 +20,4 @@ export class AtpService {
     return this.agent;
   }
 }
+
